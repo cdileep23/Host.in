d44302c2ffe707cf333e6684a8485aad8628dc0b
dileep <cdileepkumar22@gmail.com>
2025-04-26 00:44:30 +0530

    initial blueprint

--- components/extracomponents/Header.tsx
+++ components/extracomponents/Header.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
 
-const Header = () => {
+
+
+const Header = async() => {
+ 
   return (
     <nav className="sticky top-0 w-full border-b border-gray-200 bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
